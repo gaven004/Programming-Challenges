8d21d7a611214d3a6e556fcee795a99cd14d3d30
Gaven <gaven_yeung@163.com>
2018-12-24 21:19:11 +0800

    效率优化

--- 2/crypt-kicker/crypt-kicker.c
+++ 2/crypt-kicker/crypt-kicker.c
@@ -30,8 +30,7 @@ int is_unique(char words[][MAX_WORD_LENGTH + 1], int n) {
   return 1;
 }
 
-int split(const char *text, char words[][MAX_WORD_LENGTH + 1], int lEWords[],
-          int *n) {
+int split(const char *text, char words[][MAX_WORD_LENGTH + 1], int *n) {
   int l;
   char *p;
 
@@ -40,7 +39,6 @@ int split(const char *text, char words[][MAX_WORD_LENGTH + 1], int lEWords[],
     if (*text == ' ' || *text == '\n') {
       *p = '\0';
       if (is_unique(words, *n)) {
-        lEWords[*n] = l;
         (*n)++;
       }
       p = words[*n], l = 0;
@@ -54,7 +52,6 @@ int split(const char *text, char words[][MAX_WORD_LENGTH + 1], int lEWords[],
   if (l) {
     *p = '\0';
     if (is_unique(words, *n)) {
-      lEWords[*n] = l;
       (*n)++;
     }
   }
@@ -62,28 +59,8 @@ int split(const char *text, char words[][MAX_WORD_LENGTH + 1], int lEWords[],
   return (*n);
 }
 
-int is_remap(const char map[]) {
-  int i, k;
-  char rmap[26];
-
-  memset(rmap, 0, 26);
-
-  for (i = 0; i < 26; i++) {
-    if (map[i]) {
-      k = map[i] - 'a';
-
-      if (rmap[k]) {
-        return 1;
-      }
-
-      rmap[k] = 'a' + i;
-    }
-  }
-
-  return 0;
-}
-
-int try_map(const char *ciphertext, const char *dword, char map[], int l) {
+int try_map(const char *ciphertext, const char *dword, char map[], char rmap[],
+            int l) {
   int i, k;
 
   for (i = 0; i < l; i++, ciphertext++, dword++) {
@@ -95,37 +72,39 @@ int try_map(const char *ciphertext, const char *dword, char map[], int l) {
       }
     } else {
       map[k] = *dword;
-    }
-  }
 
-  if (is_remap(map)) {
-    return 0;
+      k = *dword - 'a';
+      if (rmap[k]) {
+        return 0;
+      }
+      rmap[k] = *ciphertext;
+    }
   }
 
   return 1;
 }
 
-int decrypt(const char aEWords[][MAX_WORD_LENGTH + 1], const int lEWords[],
-            char map[], int k, int n) {
+int decrypt(const char aEWords[][MAX_WORD_LENGTH + 1], char map[], char rmap[],
+            int k, int n) {
   int i, l, r;
-  char nmap[26];
+  char nmap[26], nrmap[26];
 
-  l = lEWords[k];
+  l = strlen(aEWords[k]);
 
   for (i = 0; i < nDWords[l]; i++) {
-    memcpy(nmap, map, 26);
+    memcpy(nmap, map, 26), memcpy(nrmap, rmap, 26);
 
     /* 尝试匹配 aEWords[k] 与 dict[l][i] */
-    if (try_map(aEWords[k], dict[l][i], nmap, l)) {
+    if (try_map(aEWords[k], dict[l][i], nmap, nrmap, l)) {
       /* 最后一个单词，返回成功 */
       if (k + 1 == n) {
-        memcpy(map, nmap, 26);
+        memcpy(map, nmap, 26), memcpy(rmap, nrmap, 26);
         return 1;
       }
 
       /* 否则解密下一个单词 */
-      if (decrypt(aEWords, lEWords, nmap, k + 1, n)) {
-        memcpy(map, nmap, 26);
+      if (decrypt(aEWords, nmap, nrmap, k + 1, n)) {
+        memcpy(map, nmap, 26), memcpy(rmap, nrmap, 26);
         return 1;
       }
     }
@@ -137,8 +116,8 @@ int decrypt(const char aEWords[][MAX_WORD_LENGTH + 1], const int lEWords[],
 int main() {
   int nEWords; /* number of encrypted words */
   char aEWords[MAX_LINE_LENGTH][MAX_WORD_LENGTH + 1]; /* 密文单词 */
-  int lEWords[MAX_LINE_LENGTH]; /* 密文单词的长度 */
-  char mapping[26];
+  int lEWords[MAX_LINE_LENGTH];   /* 密文单词的长度 */
+  char mapping[26], rmapping[26]; /*  */
 
   int i, l, n, result;
 
@@ -162,11 +141,14 @@ int main() {
   /* 读入密文 */
   while (fgets(buff, MAX_LINE_LENGTH + 1, stdin) != NULL) {
 
-    split(buff, aEWords, lEWords, &nEWords);
+    split(buff, aEWords, &nEWords);
+    /* 排序后提高匹配的效率 */
+    qsort(aEWords, nEWords, MAX_WORD_LENGTH + 1,
+          (int (*)(const void *, const void *))strcmp);
 
     /* 解密 */
-    memset(mapping, 0, 26);
-    result = decrypt(aEWords, lEWords, mapping, 0, nEWords);
+    memset(mapping, 0, 26), memset(rmapping, 0, 26);
+    result = decrypt(aEWords, mapping, rmapping, 0, nEWords);
 
     /* 输出 */
     for (i = 0; buff[i]; i++) {
