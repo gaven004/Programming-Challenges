6ec868c06ee7eca98708b3451ed6a88db43f1be2
Gaven <gaven_yeung@163.com>
2018-12-31 23:40:53 +0800

    改用BFS算法

--- 2/erdos-numbers/erdos-numbers.c
+++ 2/erdos-numbers/erdos-numbers.c
@@ -5,17 +5,72 @@
 
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define MAX_NUMBER_OF_AUTHORS 10000
-#define MAX_LENGTH_OF_NAME 101
-#define BUFFER_LENGTH 10001
+#define MAX_NUMBER_OF_AUTHORS (10000)
+#define MAX_LENGTH_OF_NAME (101)
+#define BUFFER_LENGTH (10001)
+#define MAX_LINKS (501)
+
+typedef struct {
+  unsigned capacity;
+  int *items; /* body of queue */
+  int head;   /* position of head element */
+  int tail;   /* position of tail element */
+  int size;   /* number of queue elements */
+} queue_t;
 
 int nAuthors;
 char authors[MAX_NUMBER_OF_AUTHORS][MAX_LENGTH_OF_NAME];
 int graph[MAX_NUMBER_OF_AUTHORS][MAX_NUMBER_OF_AUTHORS];
 int distance[MAX_NUMBER_OF_AUTHORS], visited[MAX_NUMBER_OF_AUTHORS];
+queue_t *queue;
+
+queue_t *create_queue(unsigned capacity) {
+  queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
+  queue->capacity = capacity;
+  queue->size = queue->head = queue->tail = 0;
+  queue->items = (int *)malloc(queue->capacity * sizeof(int));
+  return queue;
+}
+
+void clear_queue(queue_t *queue) {
+  queue->size = queue->head = queue->tail = 0;
+}
+
+int is_full(queue_t *queue) { return queue->size == queue->capacity; }
+
+int is_empty(queue_t *queue) { return queue->size == 0; }
+
+void en_queue(queue_t *queue, int x) {
+  if (is_full(queue)) {
+    return;
+  }
+
+  queue->items[queue->tail++] = x;
+  if (queue->tail == queue->capacity) {
+    queue->tail = 0;
+  }
+  queue->size++;
+}
+
+int de_queue(queue_t *queue) {
+  int x;
+
+  if (is_empty(queue)) {
+    return INT_MIN;
+  }
+
+  x = queue->items[queue->head++];
+  if (queue->head == queue->capacity) {
+    queue->head = 0;
+  }
+  queue->size--;
+
+  return (x);
+}
 
 int index_of(const char *name) {
   int i;
@@ -71,14 +126,18 @@ void add_authors(char *buff) {
   }
 }
 
-void calculate() {
-  int i, v, current, shortest;
+void init_4_calc() {
+  int i;
 
   distance[0] = 0, visited[0] = 0;
   for (i = 1; i < nAuthors; i++) {
     distance[i] = INT_MAX, visited[i] = 0;
   }
+}
 
+void calculate_dijkstra() {
+  int i, v, current, shortest;
+  init_4_calc();
   current = 0;
   while (!visited[current]) {
     for (i = 0; i < nAuthors; i++) {
@@ -104,13 +163,23 @@ void calculate() {
   }
 }
 
-void trim(char *buff) {
-  while (*buff) {
-    if (*buff == '\n') {
-      *buff = 0;
-      return;
+void calculate_bfs() {
+  int i, v, current, shortest;
+  init_4_calc();
+  en_queue(queue, 0);
+  while (!is_empty(queue)) {
+    current = de_queue(queue);
+
+    for (i = 0; i < nAuthors; i++) {
+      if (graph[current][i] && !visited[i]) {
+        if (distance[i] > distance[current] + 1) {
+          distance[i] = distance[current] + 1;
+        }
+        en_queue(queue, i);
+      }
     }
-    buff++;
+
+    visited[current] = 1;
   }
 }
 
@@ -123,6 +192,7 @@ int main() {
 
   start = clock();
 
+  queue = create_queue(MAX_NUMBER_OF_AUTHORS * MAX_LINKS);
   strcpy(authors[0], "Erdos, P.");
 
   for (scanf("%d", &scenarios), s = 1; s <= scenarios; s++) {
@@ -140,7 +210,7 @@ int main() {
       add_authors(buff);
     }
 
-    calculate();
+    calculate_bfs();
 
     printf("Scenario %d\n", s);
     for (i = 0; i < N; i++) {
@@ -155,7 +225,7 @@ int main() {
   }
 
   end = clock();
-	printf("\nElapsed: %ld\n", end - start);
+  printf("\nElapsed: %ld\n", end - start);
 
   return 0;
 }
