64c1c8d98c486f078b8ba88ea8e13ea6611deae8
Gaven <gaven_yeung@163.com>
2022-07-16 21:43:10 +0800

    Add WERTYU

---
+++ 3/WERTYU/WERTYU.c
@@ -0,0 +1,70 @@
+/*
+ * The 3n + 1 Problem
+ * PC/UVa IDs: 110101/100
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(void)
+{
+    char MAPPING[128];
+    MAPPING['1'] = '`';
+    MAPPING['2'] = '1';
+    MAPPING['3'] = '2';
+    MAPPING['4'] = '3';
+    MAPPING['5'] = '4';
+    MAPPING['6'] = '5';
+    MAPPING['7'] = '6';
+    MAPPING['8'] = '7';
+    MAPPING['9'] = '8';
+    MAPPING['0'] = '9';
+    MAPPING['-'] = '0';
+    MAPPING['='] = '-';
+    MAPPING['W'] = 'Q';
+    MAPPING['E'] = 'W';
+    MAPPING['R'] = 'E';
+    MAPPING['T'] = 'R';
+    MAPPING['Y'] = 'T';
+    MAPPING['U'] = 'Y';
+    MAPPING['I'] = 'U';
+    MAPPING['O'] = 'I';
+    MAPPING['P'] = 'O';
+    MAPPING['['] = 'P';
+    MAPPING[']'] = '[';
+    MAPPING['\\'] = ']';
+    MAPPING['S'] = 'A';
+    MAPPING['D'] = 'S';
+    MAPPING['F'] = 'D';
+    MAPPING['G'] = 'F';
+    MAPPING['H'] = 'G';
+    MAPPING['J'] = 'H';
+    MAPPING['K'] = 'J';
+    MAPPING['L'] = 'K';
+    MAPPING[';'] = 'L';
+    MAPPING['\''] = ';';
+    MAPPING['X'] = 'Z';
+    MAPPING['C'] = 'X';
+    MAPPING['V'] = 'C';
+    MAPPING['B'] = 'V';
+    MAPPING['N'] = 'B';
+    MAPPING['M'] = 'N';
+    MAPPING[','] = 'M';
+    MAPPING['.'] = ',';
+    MAPPING['/'] = '.';
+    MAPPING[' '] = ' ';
+    MAPPING['\r'] = '\r';
+    MAPPING['\n'] = '\n';
+
+    char c;
+
+    /* read data */
+    while ((c = getchar()) != EOF)
+    {
+        putchar(MAPPING[c]);
+    }
+
+    return EXIT_SUCCESS;
+}
