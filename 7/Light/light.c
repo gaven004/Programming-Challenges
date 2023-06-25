3b7fbd9f77a0f42f29a4693ace1556d1fb244049
Gaven <gaven_yeung@163.com>
2023-06-25 18:06:21 +0800

    UPDATE: 7.6.1 Light, More Light

--- 7/Light/light.c
+++ 7/Light/light.c
@@ -5,22 +5,9 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
+#include <math.h>
 
 #define MAXL 20
-
-unsigned int T[11] = {0, 3, 9, 31, 99, 316, 999, 3162, 10000, 31622, 65536};
-
-int is_perfect_square_int(unsigned int n, int l) {
-    long long x = T[l];
-
-    /* 牛顿迭代法 */
-    while (x * x > n) {
-        x = (x + n / x) >> 1;
-    }
-
-    return x * x == n;
-}
 
 int is_perfect_square(char buff[MAXL], int l) {
     char c, b;
@@ -32,7 +19,7 @@ int is_perfect_square(char buff[MAXL], int l) {
      */
 
     c = buff[l - 1];
-    if (c == 2 || c == 3 || c == 7 || c == 8) {
+    if (c == '2' || c == '3' || c == '7' || c == '8') {
         return 0;
     }
 
@@ -40,25 +27,25 @@ int is_perfect_square(char buff[MAXL], int l) {
         b = buff[l - 2];
 
         switch (c) {
-            case 0:
-                if (b != 0) {
+            case '0':
+                if (b != '0') {
                     return 0;
                 }
                 break;
-            case 5:
-                if (b != 2) {
+            case '5':
+                if (b != '2') {
                     return 0;
                 }
                 break;
-            case 6:
-                if (c == 2 || c == 4 || c == 6 || c == 8 || c == 0) {
+            case '6':
+                if (b == '2' || b == '4' || b == '6' || b == '8' || b == '0') {
                     return 0;
                 }
                 break;
-            case 1:
-            case 4:
-            case 9:
-                if (c == 1 || c == 3 || c == 5 || c == 7 || c == 9) {
+            case '1':
+            case '4':
+            case '9':
+                if (b == '1' || b == '3' || b == '5' || b == '7' || b == '9') {
                     return 0;
                 }
                 break;
@@ -67,28 +54,13 @@ int is_perfect_square(char buff[MAXL], int l) {
 
     sscanf(buff, "%u", &n);
 
-    if (c == 0 || c == 4 || c == 6) {
-        if (c % 4 != 0) {
-            return 0;
-        }
-    }
-
-    if (c == 1 || c == 5 || c == 9) {
-        if (c % 8 != 1) {
-            return 0;
-        }
-    }
-
-    return is_perfect_square_int(n, l);
+    s = sqrtf(n);
+    return s * s == n;
 }
 
 int main() {
     char buff[MAXL];
     int l;
-
-    clock_t start, end;
-
-    start = clock();
 
     scanf("%s", buff), l = strlen(buff);
     while (l > 0 && buff[0] > '0') {
@@ -100,9 +72,4 @@ int main() {
 
         scanf("%s", buff), l = strlen(buff);
     }
-
-    end = clock();
-
-    printf("\nElapsed: %ld\n", end - start);
-
 }
