8d26440848ffd189113e47c3ab4f379a03c42878
Gaven <gaven_yeung@163.com>
2023-06-29 22:22:26 +0800

    UPDATE: 7.6.2 Carmichael Numbers，优化素数表生成部分

--- 7/Carmichael Numbers/cn.c
+++ 7/Carmichael Numbers/cn.c
@@ -3,54 +3,29 @@
  * PC/UVa IDs: 110702/10006
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include <stdio.h>
 
 #define MAXN 65000
 
-unsigned int n_prime_numbers;
-unsigned int P[MAXN];
-
-int is_prime_number(unsigned int n) {
-    unsigned int i, s;
-
-    if (n <= 3) {
-        return 1;
-    }
-
-    s = sqrt(n) + 1; // 由于精度问题，保险起见加1
-
-    for (i = 1; P[i] <= s && i < n_prime_numbers; ++i) {
-        if (n % P[i] == 0) {
-            return 0;
-        }
-    }
-
-    return 1;
-}
+int P[MAXN];
 
 void init_prime_number_table() {
-    unsigned int i;
+    int i, j, s;
 
-    P[0] = 2;
-    n_prime_numbers = 1;
+    P[0] = P[1] = 0;
+    for (i = 2; i < MAXN; i++) {
+        P[i] = 1;
+    }
 
-    for (i = 3; i <= MAXN; i += 2) {
-        if (is_prime_number(i)) {
-            P[n_prime_numbers] = i;
-            n_prime_numbers++;
+    s = sqrt(MAXN) + 1;
+    for (i = 2; i <= s; i++) {
+        if (P[i]) {
+            for (j = i * i; j < MAXN; j += i) {
+                P[j] = 0;
+            }
         }
     }
-}
-
-int cmp(const void *a, const void *b) {
-    return (*(int *) a - *(int *) b);
-}
-
-int is_composite_number(unsigned int n) {
-    return bsearch(&n, P, n_prime_numbers, sizeof(unsigned int), cmp) == NULL;
 }
 
 /* get a ^ x mod n */
@@ -72,7 +47,7 @@ int is_carmichael_number(unsigned int n) {
     int i;
     unsigned int s;
 
-    if (!is_composite_number(n)) {
+    if (P[n]) {
         return 0;
     }
 
@@ -87,10 +62,12 @@ int is_carmichael_number(unsigned int n) {
         return 0;
     }
 
-    for (i = 0; i < n_prime_numbers && P[i] <= n; i++) {
-        // test i ^ n mod n = i
-        if (mod(P[i], n, n) != P[i]) {
-            return 0;
+    for (i = 2; i < n; i++) {
+        if (P[i]) {
+            // test i ^ n mod n = i
+            if (mod(i, n, n) != i) {
+                return 0;
+            }
         }
     }
 
