70bc72e7c602983d8a9ba1649f3d9fd566494048
Gaven <gaven_yeung@163.com>
2023-06-30 00:03:09 +0800

    UPDATE: 7.6.2 Carmichael Numbers，优化素数表生成部分

--- 7/Carmichael Numbers/cn.c
+++ 7/Carmichael Numbers/cn.c
@@ -8,21 +8,26 @@
 
 #define MAXN 65000
 
-int P[MAXN];
+int F[MAXN];
+
+unsigned int n_prime_numbers;
+unsigned int P[MAXN];
 
 void init_prime_number_table() {
     int i, j, s;
 
-    P[0] = P[1] = 0;
+    F[0] = F[1] = 0;
     for (i = 2; i < MAXN; i++) {
-        P[i] = 1;
+        F[i] = 1;
     }
 
-    s = sqrt(MAXN) + 1;
+    s = sqrt(MAXN) + 1, n_prime_numbers = 0;
     for (i = 2; i <= s; i++) {
-        if (P[i]) {
+        if (F[i]) {
+            P[n_prime_numbers] = i;
+            n_prime_numbers++;
             for (j = i * i; j < MAXN; j += i) {
-                P[j] = 0;
+                F[j] = 0;
             }
         }
     }
@@ -44,10 +49,10 @@ unsigned int mod(unsigned int a, unsigned int x, unsigned int n) {
 }
 
 int is_carmichael_number(unsigned int n) {
-    int i;
+    unsigned int i;
     unsigned int s;
 
-    if (P[n]) {
+    if (F[n]) {
         return 0;
     }
 
@@ -62,12 +67,14 @@ int is_carmichael_number(unsigned int n) {
         return 0;
     }
 
-    for (i = 2; i < n; i++) {
-        if (P[i]) {
-            // test i ^ n mod n = i
-            if (mod(i, n, n) != i) {
-                return 0;
-            }
+    /*
+     * 只需要判断质数即可，其它的合数，可由公式 xy mod n = (x mod n)(y mod n) mod n 推导出
+     * (xy) ^ n mod n = (x ^ n * y ^ n) mod n = (x ^ n mod n)(y ^ n mod n) mod n = xy mod n = xy
+     */
+    for (i = 0; i < n_prime_numbers && P[i] <= n; i++) {
+        // test i ^ n mod n = i
+        if (mod(P[i], n, n) != P[i]) {
+            return 0;
         }
     }
 
