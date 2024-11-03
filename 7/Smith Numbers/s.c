34d13f27517e009e81e00fdd7049add495c428f1
Gaven <gaven_yeung@163.com>
2024-11-03 23:20:00 +0800

    UPDATE: 7.6.6 Smith Numbers, 小优化

--- 7/Smith Numbers/s.c
+++ 7/Smith Numbers/s.c
@@ -21,52 +21,60 @@ int check(const int n) {
     t = n, s1 = sum_digits(t), s2 = 0;
 
     i = 2;
-    while (t % i == 0) {
-        t = t / i, s2 += i;
-        if (s1 < s2) {
-            return 0;
-        }
-    }
+    if (t % i == 0) {
+        do {
+            t = t / i, s2 += i;
+            if (s1 < s2) {
+                return 0;
+            }
+        } while (t % i == 0);
 
-    if (s1 == s2) {
-        return (t == 1) ? 1 : 0;
+        if (s1 == s2) {
+            return (t == 1) ? 1 : 0;
+        }
     }
 
     i = 3;
-    while (t % i == 0) {
-        t = t / i, s2 += i;
-        if (s1 < s2) {
-            return 0;
-        }
-    }
+    if (t % i == 0) {
+        do {
+            t = t / i, s2 += i;
+            if (s1 < s2) {
+                return 0;
+            }
+        } while (t % i == 0);
 
-    if (s1 == s2) {
-        return (t == 1) ? 1 : 0;
+        if (s1 == s2) {
+            return (t == 1) ? 1 : 0;
+        }
     }
 
     l = sqrt(t) + 1, i = 5; // 由于精度问题，保险起见加1
     while (i <= l) {
-        while (t % i == 0) {
-            t = t / i, s2 += sum_digits(i);
-            if (s1 < s2) {
-                return 0;
-            }
-        }
+        if (t % i == 0) {
+            do {
+                t = t / i, s2 += sum_digits(i);
+                if (s1 < s2) {
+                    return 0;
+                }
+            } while (t % i == 0);
 
-        if (s1 == s2) {
-            return (t == 1) ? 1 : 0;
+            if (s1 == s2) {
+                return (t == 1) ? 1 : 0;
+            }
         }
 
         i += 2;
-        while (t % i == 0) {
-            t = t / i, s2 += sum_digits(i);
-            if (s1 < s2) {
-                return 0;
-            }
-        }
+        if (t % i == 0) {
+            do {
+                t = t / i, s2 += sum_digits(i);
+                if (s1 < s2) {
+                    return 0;
+                }
+            } while (t % i == 0);
 
-        if (s1 == s2) {
-            return (t == 1) ? 1 : 0;
+            if (s1 == s2) {
+                return (t == 1) ? 1 : 0;
+            }
         }
 
         i += 4;
@@ -78,6 +86,7 @@ int check(const int n) {
     }
 
     if (1 < t) {
+        // 剩余的t为质数
         if (s1 == s2 + sum_digits(t))
             return 1;
     }
@@ -95,7 +104,6 @@ int solve(int n) {
 
 int main() {
     int cases, n;
-
     scanf("%d", &cases);
     while (cases--) {
         scanf("%d", &n);
